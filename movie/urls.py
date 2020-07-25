9dfadf61d07e4960d8f12353e7b26b948259512f
10nayan <nayanjyotipatowary@gmail.com>
2020-07-25 10:05:30 +0530

    new changes

--- movie/urls.py
+++ movie/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import IndexView,MovieListView,MovieDetailView,ActionListView
+from .views import IndexView,MovieListView,MovieDetailView,genrelist_view
 urlpatterns=[
     path('', IndexView.as_view(),name='index'),
     path('list', MovieListView.as_view(),name='list'),
     path('<int:pk>/detail',MovieDetailView.as_view(),name='detail'),
-    path('list/action',ActionListView.as_view(),name='action')
+    path('list/<str:genre>',genrelist_view,name='genre')
 ]
--- movie/views.py
+++ movie/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from .models import Movies
 from django.views.generic import CreateView, DetailView,ListView,UpdateView,DeleteView
 from django.views import View
@@ -14,7 +15,9 @@ class MovieListView(ListView):
 class MovieDetailView(DetailView):
     template_name='movie/detail.html'
     queryset=Movies.objects.all()
-class ActionListView(ListView):
-    template_name='movie/list.html'
-    queryset=Movies.objects.filter(Genre__contains="Action")
-    paginate_by=8
+def genrelist_view(request,genre):
+    object_list=Movies.objects.filter(Genre__contains=genre)
+    paginator = Paginator(object_list,8)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request,'movie/list.html',{'page_obj': page_obj,'object_list':object_list})
