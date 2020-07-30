46f7eb33926bab0484186a9f61b545183acc8c38
10nayan <nayanjyotipatowary@gmail.com>
2020-07-30 14:55:27 +0530

    new

--- movie/models.py
+++ movie/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
 class Movies(models.Model):
     Director=models.CharField(max_length=30)
@@ -12,6 +12,14 @@ class Movies(models.Model):
     Language=models.CharField(max_length=20,null=True)
     Like=models.IntegerField()
     Dislike=models.IntegerField()
+    def like_this_movie(self):
+        self.Like+=1
+        self.save()
+        return reverse ('list',kwargs={})
+    def dislike_this_movie(self):
+        self.Dislike+=1
+        self.save()
+        return reverse ('list',kwargs={})
     def __str__(self):
         return f"{self.Name} {self.ReleaseYear}"
     def show_name(self):
